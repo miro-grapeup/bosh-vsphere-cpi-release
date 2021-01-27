413549c1a9729601a179eece3e2c2a74d73c541c
Julian Hjortshoj <hjortshojj@vmware.com>
2021-01-26 16:45:06 -0800

    Run BATS on nsx-t managed network
    Co-authored-by: Julian Hjortshoj <hjortshojj@vmware.com>
    Co-authored-by: Mark Stokan <mstokan@pivotal.io>
    
    [#175958448] BOSH vSphere CPI uses the policy API to create networks

--- ci/tasks/run-bats.sh
+++ ci/tasks/run-bats.sh
@@ -13,19 +13,19 @@ mkdir -p bats-config
 
 bosh int source-ci/ci/shared/bats-spec.yml \
   -v "stemcell_name=${STEMCELL_NAME}" \
-  -v network1-staticIP-2=192.168.111.156 \
-  -v network1-staticIP-1=192.168.111.157 \
-  -v network1-vCenterCIDR=192.168.111.0/24 \
-  -v network1-staticRange=[192.168.111.156-192.168.111.163] \
-  -v network1-reservedRange="['192.168.111.0-192.168.111.155' , '192.168.111.164-192.168.111.174']" \
-  -v network1-vCenterGateway=192.168.111.1 \
-  -v network1-vCenterVLAN="VM Network" \
-  -v network2-staticIP-1=192.168.111.164 \
-  -v network2-vCenterCIDR=192.168.111.0/24 \
-  -v network2-staticRange=[192.168.111.164-192.168.111.174] \
-  -v network2-reservedRange="['192.168.111.0-192.168.111.163']" \
-  -v network2-vCenterGateway=192.168.111.1 \
-  -v network2-vCenterVLAN="VM Network" > bats-config/bats-config.yml
+  -v network1-staticIP-2=30.0.2.156 \
+  -v network1-staticIP-1=30.0.2.157 \
+  -v network1-vCenterCIDR=30.0.0.0/22 \
+  -v network1-staticRange=[30.0.2.156-30.0.2.163] \
+  -v network1-reservedRange="['30.0.0.0-30.0.1.255' , '30.0.3.0-30.0.3.255']" \
+  -v network1-vCenterGateway=30.0.0.1 \
+  -v network1-vCenterVLAN="vcpi-segment" \
+  -v network2-staticIP-1=30.0.3.164 \
+  -v network2-vCenterCIDR=30.0.0.0/22 \
+  -v network2-staticRange=[30.0.3.164-30.0.3.174] \
+  -v network2-reservedRange="['30.0.0.0-30.0.2.255']" \
+  -v network2-vCenterGateway=30.0.0.1 \
+  -v network2-vCenterVLAN="vcpi-segment" > bats-config/bats-config.yml
 
 source director-state/director.env
 export BAT_PRIVATE_KEY="$(bosh int director-state/creds.yml --path=/jumpbox_ssh/private_key)"
